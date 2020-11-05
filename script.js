c001f3b6bcb88c4c1491524790746676ffda98f0
sgir <sathya.prabha.girish@gmail.com>
2020-11-05 16:09:43 -0500

    create a model(NN)
     - creates the model and displays the layers

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 /**
- * 1. Load Data and display the chart
+ * 1. Load Data, Prepare Feature Set and Visualize 
  */
 
 document.addEventListener('DOMContentLoaded', fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', fetch('https://storage.googleapis.
             console.log('Here is your cleaned dataset ', cleanedDataset)
             
             // Load and plot the original input data that we are going to train on
+            // cleanedDataset is an Array of objects: eg. [{mpg: 18, hp: 130},..]
             const values = cleanedDataset.map(d => ({
             x: d.hp,
             y: d.mpg,
@@ -41,12 +42,38 @@ document.addEventListener('DOMContentLoaded', fetch('https://storage.googleapis.
                 xLabel: 'Horsepower',
                 yLabel: 'MPG',
                 height: 300
-            }
-            );
+            });
 
+            // A Model is a collection of layers, loss fn, optimziers - A Neural Network, really.
+            const model = createModel()
+            tfvis.show.modelSummary({name: 'Model Summary'}, model);
         })
     })
 );
 
   
+  /**
+ * 2. Create a Model / Neural Network
+ * 
+ */
   
+
+ function createModel(){
+    // these layered representations(which are intiated with random weights) are learned over serveral loops via models called neural networks that eventually gives us the target representation.
+
+    //instantiate a sequential model
+    const model = tf.sequential();
+
+    // A dense layer is a type of layer that multiplies its inputs by a matrix (called weights) and then adds a number (called the bias) to the result. 
+    // The inputShape is [1] because we have 1 number as our input (the horsepower of a given car).
+    // units sets the weight of the input features (representative input)
+    // Features - https://stackoverflow.com/questions/30669854/what-is-the-definition-of-feature-in-neural-network#:~:text=Features%20in%20a%20neural%20network,not%20the%20hidden%20layer%20nodes. 
+    model.add(tf.layers.dense(
+        {inputShape: [1], units:1, useBias:true}));
+
+    // Add an output layer
+    model.add(tf.layers.dense({units: 1, useBias: true}));
+
+    return model;
+
+ }
